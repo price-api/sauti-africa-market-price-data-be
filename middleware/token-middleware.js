f85568a5e4f014798fac32c6393f3e0968b0cccf
Michael Hart <michael@mhart.dev>
2020-02-11 12:37:31 -0500

    Auth0 endpoints updated

--- middleware/token-middleware.js
+++ middleware/token-middleware.js
@@ -1,15 +1,14 @@
-const jwt = require('express-jwt')
-const jwks = require('jwks-rsa')
+const jwt = require('express-jwt');
+const jwks = require('jwks-rsa');
 
 module.exports = jwt({
-  secret: jwks.expressJwtSecret({
-    cache: true,
-    rateLimit: true,
-    jwksRequestsPerMinute: 10,
-    jwksUri:
-      'https://sauti-africa-market-prices.auth0.com/.well-known/jwks.json'
-  }),
-  audience: 'https://sauti-africa-market-prices.auth0.com/api/v2/',
-  issuer: 'https://sauti-africa-market-prices.auth0.com/',
-  algorithms: ['RS256']
-})
+	secret: jwks.expressJwtSecret({
+		cache: true,
+		rateLimit: true,
+		jwksRequestsPerMinute: 10,
+		jwksUri: 'https://price-api.auth0.com.auth0.com/.well-known/jwks.json'
+	}),
+	audience: 'https://price-api.auth0.com.auth0.com/api/v2/',
+	issuer: 'https://price-api.auth0.com.auth0.com/',
+	algorithms: [ 'RS256' ]
+});
